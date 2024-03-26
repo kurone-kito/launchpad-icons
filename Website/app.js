43066a92fc1953852fc76c33a4e4cf20e5b1795f
kurone-kito <krone@kit.black>
2024-03-26 22:14:26 +0900

    chore(web): merged from yarn-project-boilerplate

--- Website/app.js
+++ Website/app.js
@@ -1,6 +1,9 @@
-import { baseLayerLuminance, StandardLuminance } from 'https://unpkg.com/@fluentui/web-components';
+import {
+  baseLayerLuminance,
+  StandardLuminance,
+} from 'https://unpkg.com/@fluentui/web-components';
 
-const LISTING_URL = "{{ listingInfo.Url }}";
+const LISTING_URL = '{{ listingInfo.Url }}';
 
 const PACKAGES = eval(`({
 {{~ for package in packages ~}}
@@ -30,33 +33,46 @@ const PACKAGES = eval(`({
 })`);
 
 const setTheme = () => {
-  const isDarkTheme = () => window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const isDarkTheme = () =>
+    window.matchMedia('(prefers-color-scheme: dark)').matches;
   if (isDarkTheme()) {
-    baseLayerLuminance.setValueFor(document.documentElement, StandardLuminance.DarkMode);
+    baseLayerLuminance.setValueFor(
+      document.documentElement,
+      StandardLuminance.DarkMode,
+    );
   } else {
-    baseLayerLuminance.setValueFor(document.documentElement, StandardLuminance.LightMode);
+    baseLayerLuminance.setValueFor(
+      document.documentElement,
+      StandardLuminance.LightMode,
+    );
   }
-}
+};
 
 (() => {
   setTheme();
 
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-    setTheme();
-  });
+  window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', () => {
+      setTheme();
+    });
 
   const packageGrid = document.getElementById('packageGrid');
 
   const searchInput = document.getElementById('searchInput');
-  searchInput.addEventListener('input', ({ target: { value = '' }}) => {
-    const items = packageGrid.querySelectorAll('fluent-data-grid-row[row-type="default"]');
-    items.forEach(item => {
+  searchInput.addEventListener('input', ({ target: { value = '' } }) => {
+    const items = packageGrid.querySelectorAll(
+      'fluent-data-grid-row[row-type="default"]',
+    );
+    items.forEach((item) => {
       if (value === '') {
         item.style.display = 'grid';
         return;
       }
       if (
-        item.dataset?.packageName?.toLowerCase()?.includes(value.toLowerCase()) ||
+        item.dataset?.packageName
+          ?.toLowerCase()
+          ?.includes(value.toLowerCase()) ||
         item.dataset?.packageId?.toLowerCase()?.includes(value.toLowerCase())
       ) {
         item.style.display = 'grid';
@@ -71,12 +87,16 @@ const setTheme = () => {
   urlBarHelpButton.addEventListener('click', () => {
     addListingToVccHelp.hidden = false;
   });
-  const addListingToVccHelpClose = document.getElementById('addListingToVccHelpClose');
+  const addListingToVccHelpClose = document.getElementById(
+    'addListingToVccHelpClose',
+  );
   addListingToVccHelpClose.addEventListener('click', () => {
     addListingToVccHelp.hidden = true;
   });
 
-  const vccListingInfoUrlFieldCopy = document.getElementById('vccListingInfoUrlFieldCopy');
+  const vccListingInfoUrlFieldCopy = document.getElementById(
+    'vccListingInfoUrlFieldCopy',
+  );
   vccListingInfoUrlFieldCopy.addEventListener('click', () => {
     const vccUrlField = document.getElementById('vccListingInfoUrlField');
     vccUrlField.select();
@@ -88,7 +108,11 @@ const setTheme = () => {
   });
 
   const vccAddRepoButton = document.getElementById('vccAddRepoButton');
-  vccAddRepoButton.addEventListener('click', () => window.location.assign(`vcc://vpm/addRepo?url=${encodeURIComponent(LISTING_URL)}`));
+  vccAddRepoButton.addEventListener('click', () =>
+    window.location.assign(
+      `vcc://vpm/addRepo?url=${encodeURIComponent(LISTING_URL)}`,
+    ),
+  );
 
   const vccUrlFieldCopy = document.getElementById('vccUrlFieldCopy');
   vccUrlFieldCopy.addEventListener('click', () => {
@@ -102,15 +126,15 @@ const setTheme = () => {
   });
 
   const rowMoreMenu = document.getElementById('rowMoreMenu');
-  const hideRowMoreMenu = e => {
+  const hideRowMoreMenu = (e) => {
     if (rowMoreMenu.contains(e.target)) return;
     document.removeEventListener('click', hideRowMoreMenu);
     rowMoreMenu.hidden = true;
-  }
+  };
 
   const rowMenuButtons = document.querySelectorAll('.rowMenuButton');
-  rowMenuButtons.forEach(button => {
-    button.addEventListener('click', e => {
+  rowMenuButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
       if (rowMoreMenu?.hidden) {
         rowMoreMenu.style.top = `${e.clientY + e.target.clientHeight}px`;
         rowMoreMenu.style.left = `${e.clientX - 120}px`;
@@ -119,7 +143,7 @@ const setTheme = () => {
         const downloadLink = rowMoreMenu.querySelector('#rowMoreMenuDownload');
         const downloadListener = () => {
           window.open(e?.target?.dataset?.packageUrl, '_blank');
-        }
+        };
         downloadLink.addEventListener('change', () => {
           downloadListener();
           downloadLink.removeEventListener('change', downloadListener);
@@ -133,38 +157,53 @@ const setTheme = () => {
   });
 
   const packageInfoModal = document.getElementById('packageInfoModal');
-  const packageInfoModalClose = document.getElementById('packageInfoModalClose');
+  const packageInfoModalClose = document.getElementById(
+    'packageInfoModalClose',
+  );
   packageInfoModalClose.addEventListener('click', () => {
     packageInfoModal.hidden = true;
   });
 
   // Fluent dialogs use nested shadow-rooted elements, so we need to use JS to style them
   const modalControl = packageInfoModal.shadowRoot.querySelector('.control');
-  modalControl.style.maxHeight = "90%";
+  modalControl.style.maxHeight = '90%';
   modalControl.style.transition = 'height 0.2s ease-in-out';
   modalControl.style.overflowY = 'hidden';
 
   const packageInfoName = document.getElementById('packageInfoName');
   const packageInfoId = document.getElementById('packageInfoId');
   const packageInfoVersion = document.getElementById('packageInfoVersion');
-  const packageInfoDescription = document.getElementById('packageInfoDescription');
+  const packageInfoDescription = document.getElementById(
+    'packageInfoDescription',
+  );
   const packageInfoAuthor = document.getElementById('packageInfoAuthor');
-  const packageInfoDependencies = document.getElementById('packageInfoDependencies');
+  const packageInfoDependencies = document.getElementById(
+    'packageInfoDependencies',
+  );
   const packageInfoKeywords = document.getElementById('packageInfoKeywords');
   const packageInfoLicense = document.getElementById('packageInfoLicense');
 
   const rowAddToVccButtons = document.querySelectorAll('.rowAddToVccButton');
   rowAddToVccButtons.forEach((button) => {
-    button.addEventListener('click', () => window.location.assign(`vcc://vpm/addRepo?url=${encodeURIComponent(LISTING_URL)}`));
+    button.addEventListener('click', () =>
+      window.location.assign(
+        `vcc://vpm/addRepo?url=${encodeURIComponent(LISTING_URL)}`,
+      ),
+    );
   });
 
-  const rowPackageInfoButton = document.querySelectorAll('.rowPackageInfoButton');
+  const rowPackageInfoButton = document.querySelectorAll(
+    '.rowPackageInfoButton',
+  );
   rowPackageInfoButton.forEach((button) => {
-    button.addEventListener('click', e => {
+    button.addEventListener('click', (e) => {
       const packageId = e.target.dataset?.packageId;
       const packageInfo = PACKAGES?.[packageId];
       if (!packageInfo) {
-        console.error(`Did not find package ${packageId}. Packages available:`, PACKAGES);
+        console.error(
+          `Did not find package ${packageId}. Packages available:`,
+          PACKAGES,
+        );
         return;
       }
 
@@ -180,7 +219,7 @@ const setTheme = () => {
       } else {
         packageInfoKeywords.parentElement.classList.remove('hidden');
         packageInfoKeywords.innerHTML = null;
-        packageInfo.keywords.forEach(keyword => {
+        packageInfo.keywords.forEach((keyword) => {
           const keywordDiv = document.createElement('div');
           keywordDiv.classList.add('me-2', 'mb-2', 'badge');
           keywordDiv.textContent = keyword;
@@ -213,7 +252,9 @@ const setTheme = () => {
     });
   });
 
-  const packageInfoVccUrlFieldCopy = document.getElementById('packageInfoVccUrlFieldCopy');
+  const packageInfoVccUrlFieldCopy = document.getElementById(
+    'packageInfoVccUrlFieldCopy',
+  );
   packageInfoVccUrlFieldCopy.addEventListener('click', () => {
     const vccUrlField = document.getElementById('packageInfoVccUrlField');
     vccUrlField.select();
@@ -224,7 +265,9 @@ const setTheme = () => {
     }, 1000);
   });
 
-  const packageInfoListingHelp = document.getElementById('packageInfoListingHelp');
+  const packageInfoListingHelp = document.getElementById(
+    'packageInfoListingHelp',
+  );
   packageInfoListingHelp.addEventListener('click', () => {
     addListingToVccHelp.hidden = false;
   });
