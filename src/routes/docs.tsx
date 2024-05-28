c40825f8d68f65a481edb8a72764779beee44f12
kurone-kito <krone@kit.black>
2024-05-28 10:14:36 +0900

    feat(web): implemented the pages

--- src/routes/docs.tsx
+++ src/routes/docs.tsx
@@ -1,11 +1,32 @@
 import type { Component } from 'solid-js';
+import { Show } from 'solid-js';
+import { useMd } from '../modules/useMd.js';
 
 /**
  * The documents page.
  * @returns The component.
  */
-const Docs: Component = () => (
-  <main class="article-container">TODO: Add the content here.</main>
-);
+const Docs: Component = () => {
+  const gettingStarted = useMd('gettingStarted');
+  const manuallyInstallation = useMd('manuallyInstallation');
+  return (
+    <>
+      <Show
+        when={gettingStarted()}
+        fallback={<article class="article-container">Loading...</article>}
+      >
+        <h2 class="article-container heading-root pt-10">Getting started</h2>
+        <article
+          class="prose-article prose-a:rounded prose-a:border-2 prose-a:no-underline prose-a:border-neutral-500 dark:prose-a:border-neutral-400 prose-a:px-6 prose-a:pb-[6px] prose-a:my-2 prose-a:pt-2 prose-a:font-medium prose-a:uppercase prose-a:text-neutral-500 dark:prose-a:text-neutral-400 prose-a:transition prose-a:duration-150 prose-a:ease-in-out hover:prose-a:border-neutral-800 hover:prose-a:text-neutral-800 dark:hover:prose-a:border-neutral-200 dark:hover:prose-a:text-neutral-200 focus:prose-a:border-neutral-300 focus:prose-a:text-neutral-200 focus:prose-a:outline-none focus:prose-a:ring-0 active:prose-a:border-neutral-300 active:prose-a:text-neutral-200 dark:prose-a:hover:bg-neutral-600 dark:prose-a:focus:bg-neutral-600"
+          innerHTML={gettingStarted() ?? ''}
+        />
+        <article
+          class="prose-anchor prose-article"
+          innerHTML={manuallyInstallation() ?? ''}
+        />
+      </Show>
+    </>
+  );
+};
 
 export default Docs;
--- src/routes/explore.tsx
+++ src/routes/explore.tsx
@@ -1,11 +1,38 @@
 import type { Component } from 'solid-js';
+import { For } from 'solid-js';
+import { IconItem } from '../components/atoms/IconItem.js';
+import { IconsLength } from '../components/organisms/IconsLength.js';
+import { useIcons } from '../modules/useIcons.js';
 
 /**
  * The explore page.
  * @returns The component.
  */
-const Explore: Component = () => (
-  <main class="article-container">TODO: Add the content here.</main>
-);
+const Explore: Component = () => {
+  const icons = useIcons();
+  return (
+    <>
+      <article class="article-container">
+        <h2 class="heading-root">Icons explorer</h2>
+        <p class="text-lg font-light py-4 dark:text-neutral-300">
+          The Launchpad Icons provide&nbsp;
+          <strong class="font-bold">
+            <IconsLength /> icons
+          </strong>
+          . The&nbsp;
+          <marker class="border-2 border-primary-500 rounded-md p-0.5 dark:border-primary-400">
+            highlighted
+          </marker>
+          &nbsp;icons are those added in the latest version.
+        </p>
+        <div class="py-4 gap-x-2 gap-y-2 lg:gap-x-4 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-9">
+          <For each={icons()} fallback={<div>Loading...</div>}>
+            {(name) => <IconItem name={name} />}
+          </For>
+        </div>
+      </article>
+    </>
+  );
+};
 
 export default Explore;
