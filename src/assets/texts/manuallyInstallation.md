04a6d6422119531489d9a8f84115354001cefcc0
kurone-kito <krone@kit.black>
2024-05-28 10:14:36 +0900

    feat(web): implemented the pages

---
+++ src/assets/texts/manuallyInstallation.md
@@ -0,0 +1,21 @@
+<!-- markdownlint-disable MD033 MD041 -->
+
+### Manually installation (not recommended)
+
+#### 1. Download the package
+
+Download the package from the
+[latest release](https://github.com/kurone-kito/launchpad-icons/releases).
+
+#### 2. Manually resolve the dependencies
+
+The package requires the following dependencies:
+
+- [<span translate="no">Unity Vector Graphics (com.unity.vectorgraphics)</span>](https://docs.unity3d.com/Packages/com.unity.vectorgraphics@2.0/manual/index.html)
+  `>=2.0.0-preview.24`
+
+#### 3. Import the package to your project
+
+1. Open your Unity project
+2. Drag and drop the downloaded package into the Unity Editor
+3. Click on the “Import” button
--- src/routes/pricing.tsx
+++ src/routes/pricing.tsx
@@ -1,11 +1,53 @@
 import type { Component } from 'solid-js';
+import { Show } from 'solid-js';
+import { PricingCard } from '../components/atoms/PricingCard.js';
+import { useMd } from '../modules/useMd.js';
 
 /**
  * The explore page.
  * @returns The component.
  */
-const Pricing: Component = () => (
-  <main class="article-container">TODO: Add the content here.</main>
-);
+const Pricing: Component = () => {
+  const md = useMd('pricing');
+  return (
+    <>
+      <section class="article-container">
+        <h2 class="heading-root">Pricing</h2>
+        <div class="grid gap-8 pb-8 md:grid-cols-2 lg:grid-cols-3">
+          <PricingCard
+            title="Free"
+            subscribe="Already subscribed"
+            features={[
+              'Unrestricted use under CC BY-NC 4.0 license',
+              'Community based support',
+            ]}
+          />
+          <PricingCard
+            title="Basic"
+            subscribe="It's the thought that counts"
+            features={[
+              'All features from the Free plan',
+              'Willingness to make more use of the Launchpad Icons',
+            ]}
+          />
+          <PricingCard
+            title="Pro"
+            subscribe="It's the thought that counts"
+            features={[
+              'All features from the Basic plan',
+              'Awareness as a professional user',
+            ]}
+          />
+        </div>
+        <Show
+          when={md()}
+          fallback={<article class="article-container">Loading...</article>}
+        >
+          <article class="prose-anchor prose-article" innerHTML={md() ?? ''} />
+        </Show>
+      </section>
+    </>
+  );
+};
 
 export default Pricing;
