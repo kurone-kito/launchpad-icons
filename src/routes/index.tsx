81824c1588e5fbab49aaa0ad21334b9fc1922c75
kurone-kito <krone@kit.black>
2024-06-01 21:29:22 +0900

    feat(web): added the empty pages

---
+++ src/routes/index.tsx
@@ -0,0 +1,7 @@
+import type { Component, JSX } from 'solid-js';
+
+const Index: Component = (): JSX.Element => (
+  <main class="article-container">TODO: Add the content here.</main>
+);
+
+export default Index;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export default { content: ['./src/**/*.{html,js,jsx,ts,tsx}'] };
+export default {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Helvetica Neue',
+          'Arial',
+          'Hiragino Sans',
+          'BIZ UDPGothic',
+          'Yu Gothic',
+          'YuGothic',
+          'Meiryo',
+          'sans-serif',
+        ],
+      },
+    },
+  },
+};
