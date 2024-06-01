b4fcef680c8a641d2ab3c47c6d5559268e942fdb
kurone-kito <krone@kit.black>
2024-06-01 21:29:22 +0900

    feat(web): added the empty pages

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,14 @@
+import { Router } from '@solidjs/router';
+import { FileRoutes } from '@solidjs/start/router';
 import type { Component } from 'solid-js';
+import { Suspense } from 'solid-js';
+import { base } from './constants.json';
 import './app.css';
 
-const App: Component = () => <>Hello,world!</>;
+const App: Component = () => (
+  <Router base={base} root={(props) => <Suspense>{props.children}</Suspense>}>
+    <FileRoutes />
+  </Router>
+);
 
 export default App;
---
+++ src/routes/docs.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+/**
+ * The documents page.
+ * @returns The component.
+ */
+const Docs: Component = () => (
+  <main class="article-container">TODO: Add the content here.</main>
+);
+
+export default Docs;
