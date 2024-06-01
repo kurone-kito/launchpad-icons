4f4aebb625c3ae301a52869f35a672026fa11191
kurone-kito <krone@kit.black>
2024-06-01 21:29:22 +0900

    feat(web): added the empty pages

---
+++ src/routes/explore.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+/**
+ * The explore page.
+ * @returns The component.
+ */
+const Explore: Component = () => (
+  <main class="article-container">TODO: Add the content here.</main>
+);
+
+export default Explore;
---
+++ src/routes/pricing.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+/**
+ * The explore page.
+ * @returns The component.
+ */
+const Pricing: Component = () => (
+  <main class="article-container">TODO: Add the content here.</main>
+);
+
+export default Pricing;
