78cc9ff83cc68a80c8af9ac86155ae131d2cb3a8
kurone-kito <krone@kit.black>
2024-06-29 11:07:11 +0900

    feat(web): added the resources

--- scripts/createDynamicJson.mjs
+++ scripts/createDynamicJson.mjs
@@ -1,4 +1,5 @@
-import { readdir, writeFile } from 'node:fs/promises';
+import { readdir, readFile, writeFile } from 'node:fs/promises';
+import { parse } from 'marked';
 
 /** icons directory */
 const ICONS = 'public/icons';
@@ -6,10 +7,33 @@ const ICONS = 'public/icons';
 /** destination file */
 const DEST = 'src/dynamic.json';
 
+/** texts directory */
+const MDS = 'src/assets/texts';
+
 /** icons list */
 const icons = (await readdir(ICONS, { withFileTypes: true }))
   .filter((file) => file.isFile() && file.name.endsWith('.svg'))
   .map((file) => file.name.replace(/\.svg$/, ''))
   .sort();
 
-await writeFile(DEST, JSON.stringify({ icons }, null, 2));
+/**
+ * empty texts
+ * @type {Readonly<Record<string, string>>}
+ */
+const emptyTexts = {};
+
+const texts = await (
+  await readdir(MDS, { withFileTypes: true })
+)
+  .filter((file) => file.isFile() && file.name.endsWith('.md'))
+  .reduce(
+    (acc, file) =>
+      acc.then(async (texts) => {
+        const key = file.name.replace(/\.md$/, '');
+        const text = await readFile(`${MDS}/${file.name}`, 'utf8');
+        return { ...texts, [key]: await parse(text) };
+      }),
+    Promise.resolve(emptyTexts),
+  );
+
+await writeFile(DEST, JSON.stringify({ icons, texts }, null, 2));
---
+++ src/assets/texts/gettingStarted.md
@@ -0,0 +1,21 @@
+<!-- markdownlint-disable MD033 MD041 -->
+
+### System requirements
+
+- **<span translate="no">VRChat Creator Companion</span>** (Recommended)
+- There is a bundled sample scene created in Unity _2022.3.22f1_;
+  we recommend previewing it using the same version.
+
+### Installation
+
+#### 1. Import the registry via the <span translate="no">VRChat Creator Companion (VCC)</span>
+
+[Add to VCC](vcc://vpm/addRepo?url=https%3A%2F%2Fkurone-kito.github.io%2Fvpm%2Findex.json)
+
+#### 2. Import the package to your project
+
+1. Click on the “Manage Project” button in the VCC
+2. Find the “<span translate="no">LaunchPad Icons</span>” package and
+   click on the “(+) Add package” button
+
+#### 3. Use the package, enjoy :D
