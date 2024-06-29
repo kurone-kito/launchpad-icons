5d1a6cbf26d9f5403e094a10db6f3bf84932a029
kurone-kito <krone@kit.black>
2024-06-30 02:46:51 +0900

    chore(web): added the tw-elements configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+import twElements from 'tw-elements/plugin.cjs';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './node_modules/tw-elements/js/**/*.js',
+  ],
+  plugins: [twElements],
   darkMode: 'class',
   theme: {
     extend: {
