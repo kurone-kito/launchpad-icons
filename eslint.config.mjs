dc4bdaa65ee50c544440420b6863ec583199eb4f
kurone-kito <krone@kit.black>
2024-07-02 21:23:19 +0900

    feat(node:root,web): implemented the empty website powered by solidJs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,1 @@
-import defaultConfig from '@kurone-kito/eslint-config-base';
-import tailwind from 'eslint-plugin-tailwindcss';
-
-export default [...defaultConfig, ...tailwind.configs['flat/recommended']];
+export { default } from '@kurone-kito/eslint-config-solid';
