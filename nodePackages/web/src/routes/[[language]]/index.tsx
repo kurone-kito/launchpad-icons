c5ca30d3321aee1716aba916553e8e210061c515
kurone-kito <krone@kit.black>
2024-07-03 18:09:25 +0900

    feat(node:root,web): added the empty pages

---
+++ nodePackages/web/src/routes/[[language]]/index.tsx
@@ -0,0 +1,14 @@
+import type { Component } from 'solid-js';
+
+/**
+ * The top page.
+ * @returns The component.
+ */
+const Index: Component = () => (
+  <article class="article-container">
+    <h2>Top page</h2>
+    <p>TODO: Add the content here.</p>
+  </article>
+);
+
+export default Index;
