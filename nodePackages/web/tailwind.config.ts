62c0c74b1380f2935350bed578ddd4bcb508ea0d
kurone-kito <krone@kit.black>
2024-07-03 18:09:25 +0900

    feat(node:root,web): added the empty pages

--- nodePackages/web/tailwind.config.ts
+++ nodePackages/web/tailwind.config.ts
@@ -1,6 +1,25 @@
+import daisyui from 'daisyui';
 import type { Config } from 'tailwindcss';
+// @ts-ignore
+import safeArea from 'tailwindcss-safe-area';
+
+/** The font stack for sans-serif text. */
+const sans = [
+  'Helvetica Neue',
+  'Arial',
+  'Hiragino Sans',
+  'BIZ UDPGothic',
+  'Yu Gothic',
+  'YuGothic',
+  'Meiryo',
+  'sans-serif',
+];
 
 /** The Tailwind CSS configuration. */
-const config: Config = { content: ['./src/**/*.{html,js,jsx,ts,tsx}'] };
+const config: Config = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  plugins: [daisyui, safeArea],
+  theme: { extend: { fontFamily: { sans } } },
+};
 
 export default config;
