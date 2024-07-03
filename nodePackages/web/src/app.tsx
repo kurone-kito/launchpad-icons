feb28ab0d52f37c34ff545a824b2295338c4be87
kurone-kito <krone@kit.black>
2024-07-03 18:09:25 +0900

    feat(node:root,web): added the empty pages

--- nodePackages/web/src/app.tsx
+++ nodePackages/web/src/app.tsx
@@ -1,10 +1,45 @@
+import { MetaProvider } from '@solidjs/meta';
+import { Router } from '@solidjs/router';
+import { FileRoutes } from '@solidjs/start/router';
 import type { Component } from 'solid-js';
+import { onMount, Suspense } from 'solid-js';
+import { themeChange } from 'theme-change';
 import './app.css';
 
 /**
  * The main application component.
  * @returns The component.
  */
-const App: Component = () => <h1 class="font-black">🚀 Launchpad Icons</h1>;
+const App: Component = () => {
+  onMount(() => themeChange(false));
+  return (
+    <MetaProvider>
+      <nav class="flex gap-2">
+        <h1 class="font-black">
+          <a href=".">🚀 Launchpad Icons</a>
+        </h1>
+        <ul class="flex gap-2">
+          <li>
+            <a href="docs">Docs</a>
+          </li>
+          <li>
+            <a href="explore">Explore</a>
+          </li>
+          <li>
+            <a href="pricing">Pricing</a>
+          </li>
+        </ul>
+      </nav>
+      <main class="min-h-[calc(100vh-40px-98px)]">
+        <Router
+          base={import.meta.env.SERVER_BASE_URL}
+          root={(props) => <Suspense>{props.children}</Suspense>}
+        >
+          <FileRoutes />
+        </Router>
+      </main>
+    </MetaProvider>
+  );
+};
 
 export default App;
