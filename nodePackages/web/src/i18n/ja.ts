483db382ef5457bd93707f4183ff891ff57ca9f9
kurone-kito <krone@kit.black>
2024-07-14 05:22:06 +0900

    feat(node:web): implemented the explore page

--- nodePackages/web/src/i18n/ja.ts
+++ nodePackages/web/src/i18n/ja.ts
@@ -9,6 +9,9 @@ const ja: Resources = {
     'Launchpad IconsはVRChatライクなアイコン集です。全アイコンをSVGベクター形式で提供しています。',
   docs: 'ドキュメント',
   explore: 'アイコンを探す',
+  exploreDescription:
+    'Launchpad Iconsは<strong>{{ num }}個</strong>のアイコンを提供しています。最新バージョンで新規追加したアイコンは<marker>ハイライト</marker>表示しています。',
+  exploreTitle: 'アイコンを探す',
   features: '特徴',
   features1Body: '今後{{ num }}個以上のアイコンを予定！',
   features1Heading: '{{ num }}個のアイコン',
