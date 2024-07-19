99979f19e304457c577998de4ecfb8e24a1d107e
kurone-kito <krone@kit.black>
2024-07-19 22:38:26 +0900

    feat(node:root,web): implemented the i18n feature

---
+++ nodePackages/web/src/i18n/en.ts
@@ -0,0 +1,5 @@
+import type { Resources } from './types.js';
+
+const en: Resources = { language: '🇬🇧' };
+
+export default en;
---
+++ nodePackages/web/src/i18n/types.ts
@@ -0,0 +1,5 @@
+/** Type definition for the resources object. */
+export type Resources = Readonly<Record<ResourcesKeys, string>>;
+
+/** Type definition for the resources keys. */
+export type ResourcesKeys = 'language';
