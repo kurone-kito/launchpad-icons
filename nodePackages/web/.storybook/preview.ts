a354235b349260def80726b5e6f7b6e4b311d0d6
kurone-kito <krone@kit.black>
2024-08-24 07:17:49 +0900

    test(node:root,web): added the storybook environment

---
+++ nodePackages/web/.storybook/preview.ts
@@ -0,0 +1,7 @@
+import type { Preview } from 'storybook-solidjs';
+import '../src/app.css';
+
+/** The preview configuration for Storybook. */
+const preview: Preview = { tags: ['autodocs'] };
+
+export default preview;
