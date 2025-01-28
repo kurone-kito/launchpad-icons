f53ab57c9b0fe683d96afcc4dc51a1fc4e742d77
kurone-kito <krone@kit.black>
2025-01-28 21:23:19 +0900

    feat(node:root,web): implemented the empty website powered by solidJs

---
+++ nodePackages/web/src/app.tsx
@@ -0,0 +1,10 @@
+import type { Component } from 'solid-js';
+import './app.css';
+
+/**
+ * The main application component.
+ * @returns The component.
+ */
+const App: Component = () => <h1 class="font-black">🚀 Launchpad Icons</h1>;
+
+export default App;
