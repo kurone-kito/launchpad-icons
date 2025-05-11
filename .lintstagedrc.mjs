013787617d68d78706a675a9775dda075d28750c
kurone-kito <krone@kit.black>
2025-05-11 21:06:28 +0900

    chore(node:root): migrated the linter environment to biome

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,1 +1,1 @@
-export { useEslint as default } from '@kurone-kito/lint-staged-config';
+export { useBiome as default } from '@kurone-kito/lint-staged-config';
--- eslint.config.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from '@kurone-kito/eslint-config-solid';
